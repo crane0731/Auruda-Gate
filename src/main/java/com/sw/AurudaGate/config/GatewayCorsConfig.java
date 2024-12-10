2d76c845a4afa727503e281712293c9d3abd3201
crane0731 <dlwnsgkr8318@naver.com>
2024-12-10 19:05:03 +0900

    .

--- src/main/java/com/sw/AurudaGate/config/GatewayCorsConfig.java
+++ src/main/java/com/sw/AurudaGate/config/GatewayCorsConfig.java
@@ -1,56 +1,49 @@
-package com.sw.AurudaGate.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class GatewayCorsConfig {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-
-        CorsConfiguration corsConfig = new CorsConfiguration();
-
-        // 허용할 오리진 추가
-        corsConfig.addAllowedOrigin("http://localhost:3000"); // 배포 환경
-
-        //corsConfig.addAllowedOrigin("http://14.63.178.38:3000"); // React 앱의 개발 환경
-        //corsConfig.addAllowedOrigin("https://auruda.duckdns.org"); // 배포 환경
-
-        // 허용할 HTTP 메서드
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("POST");
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("DELETE");
-        corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.addAllowedMethod("HEAD");
-        corsConfig.addAllowedMethod("PATCH");
-
-        // 허용할 헤더 설정
-        corsConfig.addAllowedHeader("*"); // 모든 요청 헤더 허용
-
-        // 응답에서 노출할 헤더
-        corsConfig.addExposedHeader("Authorization");
-        corsConfig.addExposedHeader("Cache-Control");
-        corsConfig.addExposedHeader("Content-Disposition");
-
-        // 인증 정보 허용
-        corsConfig.setAllowCredentials(true);
-
-        // 모든 경로에 대해 CORS 설정 적용
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
-}
+//package com.sw.AurudaGate.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class GatewayCorsConfig {
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//
+//        // 허용할 오리진 추가
+//        corsConfig.addAllowedOrigin("http://localhost:3000"); // 배포 환경
+//
+//        //corsConfig.addAllowedOrigin("http://14.63.178.38:3000"); // React 앱의 개발 환경
+//        //corsConfig.addAllowedOrigin("https://auruda.duckdns.org"); // 배포 환경
+//
+//        // 허용할 HTTP 메서드
+//        corsConfig.addAllowedMethod("GET");
+//        corsConfig.addAllowedMethod("POST");
+//        corsConfig.addAllowedMethod("PUT");
+//        corsConfig.addAllowedMethod("DELETE");
+//        corsConfig.addAllowedMethod("OPTIONS");
+//        corsConfig.addAllowedMethod("HEAD");
+//        corsConfig.addAllowedMethod("PATCH");
+//
+//        // 허용할 헤더 설정
+//        corsConfig.addAllowedHeader("*"); // 모든 요청 헤더 허용
+//
+//        // 응답에서 노출할 헤더
+//        corsConfig.addExposedHeader("Authorization");
+//        corsConfig.addExposedHeader("Cache-Control");
+//        corsConfig.addExposedHeader("Content-Disposition");
+//
+//        // 인증 정보 허용
+//        corsConfig.setAllowCredentials(true);
+//
+//        // 모든 경로에 대해 CORS 설정 적용
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
--- src/main/java/com/sw/AurudaGate/config/WebSecurityConfig.java
+++ src/main/java/com/sw/AurudaGate/config/WebSecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -24,11 +27,12 @@ public class WebSecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .cors(cors->cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 활성화
+                //.cors(cors->cors.disable())
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/aurudalogin/api/auruda/auth/signup", "/aurudalogin/api/auruda/auth/login", "/aurudalogin/api/auth/kakao", "/aurudalogin/api/auth/kakao/callback", "/aurudatrip/api/auruda/photo",
                                 "/aurudaarticle/api/auruda/article", "/aurudaarticle/api/auruda/comment/list/{article_id}",
-                                "/aurudatrip/api/auruda/review/{place_id}", "/aurudatrip/api/auruda/place/**", "/aurudatrip/api/auruda/latest-festivals", "/aurudatrip/api/auruda/latest-concerts","/aurudatrip/api/auruda/travel/itinerary").permitAll() // 로그인 서버의 공개 엔드포인트
+                                "/aurudatrip/api/auruda/review/{place_id}", "/aurudatrip/api/auruda/place/**", "/aurudatrip/api/auruda/latest-festivals", "/aurudatrip/api/auruda/latest-concerts").permitAll() // 로그인 서버의 공개 엔드포인트
 
                         .pathMatchers("/api/auruda/users/admin/**").hasRole("ADMIN")
 
@@ -36,12 +40,25 @@ public class WebSecurityConfig {
                                 "/aurudaarticle/api/auruda/article/me", "/aurudaarticle/api/auruda/article/recommendation/{article_id}",
                                 "/aurudaarticle/api/auruda/comment", "/aurudaarticle/api/auruda/comment/{comment_id}",
                                 "/aurudaarticle/api/auruda/comment/me", "/aurudatrip/api/auruda/review", "/aurudatrip/api/auruda/review/{review_id}",
-                                "/aurudatrip/api/auruda/storage/**", "/aurudatrip/api/auruda/travel", "/aurudaarticle/api/auruda/article/image", "/aurudatrip/api/auruda/kako/places").authenticated()
+                                "/aurudatrip/api/auruda/storage/**", "/aurudatrip/api/auruda/travel", "/aurudaarticle/api/auruda/article/image", "/aurudatrip/api/auruda/kako/places","/aurudalogin/api/auruda/image").authenticated()
 
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(new JwtAuthenticationFilter(tokenProvider), SecurityWebFiltersOrder.AUTHENTICATION); // JWT 필터 추가
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // React 앱 URL
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 인증 정보 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
